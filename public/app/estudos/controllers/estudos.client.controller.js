6f2d33903bad7fc296002986be9e0cf7be65c09f
Vittorio <vittorio.cadastro@gmail.com>
2016-06-01 19:14:22 -0300

    Testes primários com cálculo do FOB. Já é possível iniciar o desenvolvimento de um sistema.

--- public/app/estudos/controllers/estudos.client.controller.js
+++ public/app/estudos/controllers/estudos.client.controller.js
@@ -4,27 +4,28 @@
 angular.module('estudos').controller('EstudosController', ['$scope', '$routeParams', '$location', 'Produtos',
     function($scope, $routeParams, $location, Produtos) {
 
+        $scope.quantidades = [];
         $scope.produtosDoEstudo = [];
         $scope.estudo = {
             totalFob: 0
         };
-        
+
         $scope.listProdutos = function() {
             $scope.produtos = Produtos.query();
         };
         
         $scope.selecionaProdutoEstudo = function(item) {
+            item.qtd = 0;
             $scope.produtosDoEstudo.push(item);
-            iniImport();
         };
 
 
         function calculaFob(produto) {
-            return Number(produto.custo_usd) * 10;
+            return Number(produto.custo_usd) * produto.qtd;
         }
 
-
-        function iniImport() {
+        $scope.iniImport = function() {
+            $scope.estudo.totalFob = 0;
             $scope.produtosDoEstudo.forEach(function (produto) {
                 $scope.estudo.totalFob += calculaFob(produto);
             });
