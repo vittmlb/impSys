fb093640af053955886a8a425f3ff5b54a33e18b
Vittorio <vittorio.cadastro@gmail.com>
2016-06-10 21:25:20 -0300

    Implementado sistema para auxílio de cálculo do CBM dos produtos

--- public/app/produtos/controllers/produtos.client.controller.js
+++ public/app/produtos/controllers/produtos.client.controller.js
@@ -3,6 +3,40 @@
  */
 angular.module('produtos').controller('ProdutosController', ['$scope', '$routeParams', '$location', 'Produtos', '$stateParams', '$state',
     function($scope, $routeParams, $location, Produtos, $stateParams, $state) {
+
+        $scope.volCBM20 = '';
+        $scope.qtdCBM20 = '';
+        $scope.volCBM40 = '';
+        $scope.qtdCBM40 = '';
+        $scope.largura = '';
+        $scope.altura = '';
+        $scope.comprimento = '';
+        $scope.cbmProduto20 = '';
+        $scope.cbmProduto40 = '';
+        $scope.cbmProdutoMedidas = '';
+        
+        $scope.calculaCBM = function(item) {
+            if(item === 20) {
+                if ($scope.volCBM20 > 0 && $scope.qtdCBM20 > 0) {
+                    $scope.cbmProduto20 = Number($scope.volCBM20 / $scope.qtdCBM20);
+                } else {
+                    $scope.cbmProduto20 = '';
+                }
+            } else if (item === 40) {
+                if ($scope.volCBM40 > 0 && $scope.qtdCBM40 > 0) {
+                    $scope.cbmProduto40 = $scope.volCBM40 / $scope.qtdCBM40;
+                } else {
+                    $scope.cbmProduto40 = '';
+                }
+            } else if (item === 'medidas') {
+                if ($scope.largura > 0 && $scope.altura > 0 && $scope.comprimento > 0) {
+                    $scope.cbmProdutoMedidas = $scope.largura * $scope.altura * $scope.comprimento;
+                } else {
+                    $scope.cbmProdutoMedidas = '';
+                }
+            }
+        };
+
         $scope.create = function() {
             var produto = new Produtos({
                 nome: this.nome,
